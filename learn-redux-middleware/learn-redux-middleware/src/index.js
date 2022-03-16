64e2300356807ffed78c55f6976da07a3f004ee1
roum02 <roum02@ewhain.net>
2022-03-16 23:56:47 +0900

    study 정리

---
+++ learn-redux-middleware/learn-redux-middleware/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
